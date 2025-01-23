fee375a713ffb964e7d705bab2c7380ef1a28c82
mohammaddarweesh-oop <githubdarweesh@gmail.com>
2025-01-23 20:55:48 +0300

    Update CORS

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const teacherRoutes = require("./Routes/teacherRoutes");
 const authRoute = require("./Routes/authRoutes");
 const protect = require("./Middlewares/protect");
 const exportRoutes = require("./Routes/exportRoutes");
+const helmet = require("helmet");
+const cors = require("cors");
+
 require("./config/db");
 require("dotenv").config();
 
@@ -11,11 +14,22 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, 
+}));
 
 app.use("/api", authRoute);
 app.use("/api", protect, teacherRoutes);
 app.use("/api", protect, studentRoutes);
 app.use("/api", protect, exportRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 app.listen(PORT, () =>
   console.log(`Server Running on http://localhost:${PORT}`)
